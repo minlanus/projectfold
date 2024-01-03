1a27009d32982ca4f289e29d7928d5e61b7a5f50
YoungHo <timenot@naver.com>
2024-01-03 16:34:57 +0900

    "질문 상세"

--- src/main/java/kr/or/ysedu/c402/question/QuestionService.java
+++ src/main/java/kr/or/ysedu/c402/question/QuestionService.java
@@ -19,5 +19,12 @@ public class QuestionService {
 	public List<Question> getList(){
 		return this.questionRepository.findAll();
 	}
-	
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		}else {
+			throw new DataNotFoundException("question not found");
+		}
+	}
 }
