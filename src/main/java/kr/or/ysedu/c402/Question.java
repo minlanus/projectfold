c56364fa9b17df667a86d9a44566d8861bdbfad1
YoungHo <timenot@naver.com>
2024-01-03 14:59:12 +0900

    "리포지터리"

--- src/main/java/kr/or/ysedu/c402/Question.java
+++ src/main/java/kr/or/ysedu/c402/Question.java
@@ -18,19 +18,18 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Question {
-	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(length = 200)
-	private String subject;
-	
-	@Column(columnDefinition = "TEXT")
-	private String content;
-	
-	private LocalDateTime createDate;
-	
-	@OneToMany(mappedBy="question", cascade=CascadeType.REMOVE)
-	private List<Answer> answerList;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
