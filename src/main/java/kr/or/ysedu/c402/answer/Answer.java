91c04e846b51fbcef4181887958e8dcc4c4fe0ee
YoungHo <timenot@naver.com>
2024-01-09 17:24:12 +0900

    "추천기능 추가"

--- src/main/java/kr/or/ysedu/c402/answer/Answer.java
+++ src/main/java/kr/or/ysedu/c402/answer/Answer.java
@@ -1,12 +1,14 @@
 package kr.or.ysedu.c402.answer;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import kr.or.ysedu.c402.question.Question;
 import kr.or.ysedu.c402.user.SiteUser;
@@ -33,4 +35,7 @@ public class Answer {
 	private SiteUser author;
 	
 	private LocalDateTime modifyDate;
+	
+	@ManyToMany
+	Set<SiteUser> voter;
 }
